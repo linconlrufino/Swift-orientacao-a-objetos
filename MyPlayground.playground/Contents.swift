82f15bf3dae41c85cab6678ee883e203cbce3efe
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-06-28 16:26:29 -0300

    Desafios

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,5 @@
+/*
+ 
 struct ContaCorrenteStruct {
     var saldo = 0.0
     var nome: String
@@ -53,3 +55,98 @@ contaCorrenteClasse01.depositar(60)
 contaCorrenteClasse02.depositar(100)
 print("Saldo da conta corrente classe 01 = \(contaCorrenteClasse01.saldo)")
 print("Saldo da conta corrente classe 02 = \(contaCorrenteClasse02.saldo)")
+
+*/
+
+
+//Desafio 1 - Calculadora
+
+class Calculadora {
+    
+    var numero1: Double
+    var numero2: Double
+    
+    func soma() -> Double {
+        return numero1 + numero2
+    }
+    
+    func subtracao() -> Double {
+        return numero1 - numero2
+    }
+    
+    func multiplicacao() -> Double {
+        return numero1 * numero2
+    }
+    
+    func divisao() -> Double {
+        return numero1 / numero2
+    }
+    
+    init(numero1: Double, numero2: Double ){
+        self.numero1 = numero1
+        self.numero2 = numero2
+    }
+}
+
+var calculadora: Calculadora = Calculadora(numero1: 6, numero2: 4)
+
+print("Soma: \(calculadora.soma())")
+print("Subtração: \(calculadora.subtracao())")
+print("Multiplicação: \(calculadora.multiplicacao())")
+print("Divisão: \(calculadora.divisao())")
+    
+
+//Desafio 2 - Restaurante
+
+struct Restaurante {
+    var nome: String
+    var tipoDeComida: String
+    var numeroDePedidos: Int = 0
+    
+    mutating func recebePedido() -> Void {
+        self.numeroDePedidos += 1
+    }
+    
+    func calculaTotalPedidos() -> Double {
+        var valorFixo: Double = 35
+        return Double(self.numeroDePedidos) * valorFixo
+    }
+}
+
+var restautante = Restaurante(nome: "Little Boy", tipoDeComida: "Doce")
+
+restautante.recebePedido()
+restautante.recebePedido()
+restautante.recebePedido()
+restautante.recebePedido()
+
+print("Valor total dos pedidos: R$\(restautante.calculaTotalPedidos())")
+
+
+//Desafio 3 - Retângulo
+
+
+class Retangulo {
+    
+    var base: Double
+    var altura: Double
+    
+    func calcularArea() -> Double {
+        return base * altura
+    }
+    
+    func calcularperimetro() -> Double {
+        return base * 2 + altura * 2
+    }
+    
+    init(base: Double, altura: Double){
+        self.base = base
+        self.altura = altura
+    }
+}
+
+var retangulo: Retangulo = Retangulo(base: 10, altura: 20)
+
+print("Area: \(retangulo.calcularArea())")
+print("Perimetro: \(retangulo.calcularperimetro())")
+
