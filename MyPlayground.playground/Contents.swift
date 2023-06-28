4d41a4beecc9b628a6165329b07a7c1c73bcd764
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-06-27 23:33:18 -0300

    Structs

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,23 @@
-import UIKit
+struct ContaCorrente {
+    var saldo = 0.0
+    var nome: String
+    
+    mutating func sacar(_ valor: Double) -> Void {
+        saldo -= valor
+    }
+    
+    mutating func deposita(_ valor: Double) -> Void {
+        saldo += valor
+    }
+}
 
-var greeting = "Hello, playground"
+var contaCorrenteUsuarioJumenec: ContaCorrente = ContaCorrente(nome: "Jumenec")
+print(contaCorrenteUsuarioJumenec.saldo)
+contaCorrenteUsuarioJumenec.deposita(10000)
+print(contaCorrenteUsuarioJumenec.saldo)
+
+var contaCorrenteUsuarioCenas: ContaCorrente = ContaCorrente(nome: "Cenas")
+contaCorrenteUsuarioCenas.deposita(200)
+print(contaCorrenteUsuarioCenas.saldo)
+contaCorrenteUsuarioCenas.sacar(100)
+print(contaCorrenteUsuarioCenas.saldo)
