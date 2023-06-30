59c82b5d91be5d541f88a7aefc6624b281bb345b
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-06-29 23:00:24 -0300

    Enumerações

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -434,7 +434,6 @@ let texto = "Zico"
 print(texto.contaCaracteresString())
 
 
- */
 
 
 protocol Conta {
@@ -461,3 +460,57 @@ class ContaCorrente: Conta {
     }
 }
 
+ */
+
+enum FormaDePagamento {
+    case pix
+    case boleto
+    case saldoEmConta
+}
+
+class Conta {
+    var saldo = 0.0
+    var nome: String
+
+    func sacar(_ valor: Double) {
+        saldo -= valor
+    }
+
+    func depositar(_ valor: Double) {
+        saldo += valor
+    }
+    
+    func pagamentoCartao(_ tipoPagamento: FormaDePagamento){
+        switch tipoPagamento {
+        case .pix: print("O pagamento será efetuado por Pix")
+        case .boleto: print("O pagamento será efetuado por boleto")
+        case .saldoEmConta: print("O pagamento será efetuado por saldo em conta")
+        }
+    }
+
+    init(nome: String) {
+        self.nome = nome
+    }
+}
+
+var contaGiovanna = Conta(nome: "Giovanna")
+
+contaGiovanna.pagamentoCartao(.saldoEmConta)
+
+enum Mes {
+    case janeiro,
+         fevereiro,
+         marco,
+         abril,
+         maio,
+         junho,
+         julho,
+         agosto,
+         setembro,
+         outubro,
+         novembro,
+         dezembro
+}
+
+var janeiro = Mes.junho
+var feveiro: Mes = .fevereiro
