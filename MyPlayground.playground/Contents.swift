b7d41bd84ac73ad111cf7a3b537b65a521accf72
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-06-29 23:35:34 -0300

    comentando codigo

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -610,21 +610,21 @@ print(somaMoedas(moedas))
 
 //Desafio 02 - Moedas - Outra implementação
 
-enum Moeda: Int {
-  case UmCentavo = 1
-  case CincoCentavos = 5
-  case DezCentavos = 10
-  case VinteCincoCentavos = 25
-  case CinquentaCentavos = 50
-
-  static func contarMoedas(_ moedas: [Self]) -> Int {
-    var soma = 0
-    for valor in moedas {
-      soma += valor.rawValue
-    }
-    return soma
-  }
-}
-
-let moedas: [Moeda] = [.CincoCentavos, .UmCentavo, .CincoCentavos, .VinteCincoCentavos, .CincoCentavos, .DezCentavos]
-Moeda.contarMoedas(moedas)
+//enum Moeda: Int {
+//  case UmCentavo = 1
+//  case CincoCentavos = 5
+//  case DezCentavos = 10
+//  case VinteCincoCentavos = 25
+//  case CinquentaCentavos = 50
+//
+//  static func contarMoedas(_ moedas: [Self]) -> Int {
+//    var soma = 0
+//    for valor in moedas {
+//      soma += valor.rawValue
+//    }
+//    return soma
+//  }
+//}
+//
+//let moedas: [Moeda] = [.CincoCentavos, .UmCentavo, .CincoCentavos, .VinteCincoCentavos, .CincoCentavos, .DezCentavos]
+//Moeda.contarMoedas(moedas)
